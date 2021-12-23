9cdd93395a8e1fd5e5f8ae879adb22839f41359f
Amy Lim <amylim32897@gmail.com>
2021-12-23 20:49:23 +0900

    login check filter

--- src/main/java/hello/login/web/filter/LoginCheckFilter.java
+++ src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -17,33 +17,34 @@ public class LoginCheckFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
+
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
         try {
             log.info("인증 체크 필터 시작 {}", requestURI);
-
             if (isLoginCheckPath(requestURI)) {
                 log.info("인증 체크 로직 실행 {}", requestURI);
                 HttpSession session = httpRequest.getSession(false);
                 if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
                     log.info("미인증 사용자 요청 {}", requestURI);
-                    //로그인으로 redirect
+                    // 로그인으로 redirect
                     httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
-                    return; //여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
+                    return;
                 }
             }
             chain.doFilter(request, response);
         } catch (Exception e) {
-            throw e; //예외 로깅 가능 하지만, 톰캣까지 예외를 보내주어야 함
+            throw e; // 예외 로깅 가능하지만 톰캣까지 예외를 보내줘야 함
         } finally {
             log.info("인증 체크 필터 종료 {}", requestURI);
         }
     }
 
     /**
-     * 화이트 리스트의 경우 인증 체크X
+     * 화이트 리스트의 경우 인증 체크 X
      */
     private boolean isLoginCheckPath(String requestURI) {
         return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
